5468f806a3c992d06dfc64401010d58d13b2f758
AdityKumari09 <kumariadity09@gmail.com>
2023-08-16 18:39:18 +0530

    Update the Repository

--- 012/Test.c
+++ 012/Test.c
@@ -1,18 +1,35 @@
-#include <stdio.h>
+// #include <stdio.h>
+
+// int main(void)
+// {
+//     char a, b;
+//     printf("Enter 2 characters: ");
+//     scanf("%c%c%c", &a, &b, &b);
+
+//     printf("Your characters are %c and %c\n", a, b);
+
+//     return 0;
+// }
+
+
+#include<stdio.h>
 
 int main(void)
 {
-    char a, b;
-    printf("Enter 2 characters: ");
-    scanf("%c%c%c", &a, &b, &b);
+    char a;
+    printf("Enter two characters: ");
+    scanf("%c", &a);
 
-    printf("Your characters are %c and %c\n", a, b);
+    printf("your characters are %c", a);
+    
+    scanf("%c%c", &a, &a);
+    printf(" and %c\n", a);
 
     return 0;
 }
 /*
 
 Enter 2 characters: a b
-Your characters are a and b
+your characters are a and b
 
 */
--- 012/Test.java
+++ 012/Test.java
@@ -1,19 +1,39 @@
+// import java.util.Scanner;
+
+// class Test
+// {
+//    public static void main(String[] args)
+//    {
+//     Scanner sc = new Scanner(System.in);
+
+//     char a, b;
+//     System.out.print("Enter 2 characters: ");
+//     a = sc.next().charAt(0);
+//     b = sc.next().charAt(0);
+    
+//     System.out.println("your characters are " + a + " and "+ b);
+
+//    } 
+// }
+
 import java.util.Scanner;
 
 class Test
 {
    public static void main(String[] args)
    {
-    Scanner sc = new Scanner(System.in);
+      Scanner sc = new Scanner(System.in);
 
-    char a, b;
-    System.out.print("Enter 2 characters: ");
-    a = sc.next().charAt(0);
-    b = sc.next().charAt(0);
-    
-    System.out.println("your characters are " + a + " and "+ b);
+      char a;
+      System.out.print("Enter 2 characters: ");
+      a = sc.next().charAt(0);
 
-   } 
+      System.out.print("your characters are " + a);
+
+      a = sc.next().charAt(0);
+
+      System.out.println(" and " + a);
+   }
 }
 /*
 
