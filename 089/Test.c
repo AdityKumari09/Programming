b7530d5880b687c68b42a9bb5f902b8c362fee6a
AdityKumari09 <kumariadity09@gmail.com>
2023-10-12 19:36:26 +0530

    Update the Repository

--- 089/Test.c
+++ 089/Test.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 
+void reverse_array(int*);
+void swap(int* , int*);
+
 int main(void)
 {
     int arr[20];
     printf("Enter 20 integers: ");
-    
+
     for (int i = 0; i < 20; ++i)
     {
         scanf("%d", &arr[i]);
     }
 
     printf("Before reversing: ");
-    
+
     for (int i = 0; i < 20; ++i)
     {
         printf("%d ", arr[i]);
@@ -19,36 +22,76 @@ int main(void)
 
     printf("\n");
 
-    for (int i = 0; i < 20 / 2; ++i)
-    {
-        // swap arr[i] and arr[19 - i]
-
-        int temp = arr[i];
-        arr[i] = arr[19 - i];
-        arr[19 - i] = temp;
-    }
-
-    // int temp = arr[0];
-    // arr[0] = arr[4];
-    // arr[4] = temp;
-
-    // temp = arr[1];
-    // arr[1] = arr[3];
-    // arr[3] = temp;
+    reverse_array(arr);
 
     printf("After reversing: ");
-    
+
     for (int i = 0; i < 20; ++i)
     {
         printf("%d ", arr[i]);
     }
 
     printf("\n");
-
-    return 0;
 }
 
+void reverse_array(int* arr)
+{
+    for (int i = 0; i < 20 / 2; ++i)
+    {
+        swap(&(arr[i]), &(arr[19 - i]));
+    }
+}
 
+void swap(int* ptr_a, int* ptr_b)
+{
+    int temp = *ptr_a;
+    *ptr_a = *ptr_b;
+    *ptr_b = temp;
+}
+
+// #include <stdio.h>
+
+// int main(void)
+// {
+//     int arr[20];
+//     printf("Enter 20 integers: ");
+    
+//     for (int i = 0; i < 20; ++i)
+//     {
+//         scanf("%d", &arr[i]);
+//     }
+
+//     printf("Before reversing: ");
+    
+//     for (int i = 0; i < 20; ++i)
+//     {
+//         printf("%d ", arr[i]);
+//     }
+
+//     printf("\n");
+
+//     for (int i = 0; i < 20 / 2; ++i)
+//     {
+//         // swap arr[i] and arr[19 - i]
+
+        // int temp = arr[i];
+        // arr[i] = arr[19 - i];
+        // arr[19 - i] = temp;
+//     }
+
+//     printf("After reversing: ");
+    
+//     for (int i = 0; i < 20; ++i)
+//     {
+//         printf("%d ", arr[i]);
+//     }
+
+//     printf("\n");
+
+//     return 0;
+// }
+
+//POINTER NOTATION
 // #include<stdio.h>
 
 // int main(void)
